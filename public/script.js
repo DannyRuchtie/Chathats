79ff2a63d3bdd87215c5380430c0eabc9eb8caca
Danny Ruchtie <46239581+DannyRuchtie@users.noreply.github.com>
2021-05-03 19:54:13 +0200

    added visuals

--- public/script.js
+++ public/script.js
@@ -1,18 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-//    host: '/',
-// secure:true,
-//    port: '3000'
  })
-
-// const myPeer = new Peer({
- 
-//   host:'peerjs-server.herokuapp.com', 
-//   secure:true,
-//    port:443
-// })
-
 
 
 const myVideo = document.createElement('video')
@@ -21,13 +10,16 @@ myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: true
+  audio: false
 }).then(stream => {
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
+
+
+
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })
